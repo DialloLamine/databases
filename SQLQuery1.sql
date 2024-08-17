e1f5e17029f174b284746d34afd3de29ead1309c
Diallo <diallo.lamine11958@gmail.com>
2024-08-18 00:03:54 +0200

    Insertion de données

---
+++ SQLQuery1.sql
@@ -0,0 +1,5 @@
+INSERT INTO testeur (id, nom, job)
+VALUES 
+(2, 'Jane Smith', 'Project Manager'),
+(3, 'Samuel Johnson', 'Product Owner'),
+(4, 'Anna Brown', 'QA Engineer');
---
+++ SQLQuery5.sql
@@ -0,0 +1,1 @@
+INSERT INTO departments (department_name) VALUES ('Human Resources'), ('Engineering');
